b8cf5a1fcc9f678b87260c67a5b8e6c0b82147f3
cartecs <charles@charlescarter.info>
2022-10-03 17:47:31 -0500

    day complete

--- day1/day1.py
+++ day1/day1.py
@@ -1,15 +1,15 @@
 #exercise 1: print text to console
 b = "Hello!"
 a = "World!"
-
 print("Hello World!")
 print (b + " " + a)
 
 #exercise 3: count letters in input for a name
-print ("What is your name?")
+""" print ("What is your name?")
 test = input(b)
 number = len([ele for ele in test if ele.isalpha()])
-print ("Your name is ",number," letters long")
+print ("Your name is ",number," letters long") """
+
 #exercise 4: swap a to b and b to a
 a = 3
 b = 5
@@ -20,3 +20,8 @@ print ("a: ",a)
 print ("b: ",b)
 
 #exercise 5: band name generator
+print("Welcome to the band name generator")
+
+city = input("What city did you grow up in?\n")
+pet_name = input("What is the name of a pet?\n")
+print("Your band name is: " + city + " " + pet_name)
