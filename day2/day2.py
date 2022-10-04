a92d6bb0d2072550201bbe706d8ad7554ea72fd8
cartecs <charles@charlescarter.info>
2022-10-04 07:03:53 -0500

    exercise 1 & 2 day 2

--- day2/day2.py
+++ day2/day2.py
@@ -8,3 +8,32 @@ print("Hello"[4])
 print(3.14)
 #boolean
 print(True)
+
+#type conversion & type errors
+num_char = 5
+print(type(num_char))
+new_num = str(num_char)
+print(type(new_num))
+
+a = float(123)
+print(a)
+
+#Exercise 1
+#two_digit_number = input("Type a two digit number: ")
+#print(int(two_digit_number[0]) + int(two_digit_number[1]))
+
+#mathematical operations -> PEMDAS
+print (2**2) #exponent
+
+#Exercise 2
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+float_height = float(height)
+int_weight = int(weight)
+
+BMI = (int_weight/(float_height**2))
+int_BMI = int(BMI)
+
+print(f"{int_weight} / ({float_height} x {float_height} = {BMI}")
+print(f"Rounded down to: {int_BMI}")
