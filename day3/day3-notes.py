c1eb0228c2343a0f15704c5f0417e6c908400a0b
Charles <charles@cmcarter.net>
2022-10-05 06:00:09 -0500

    day 3 exercise 1

---
+++ day3/day3-notes.py
@@ -0,0 +1,17 @@
+#day 3 of 100 days of python
+#conditionals: if & else
+""" print("Welcome to the rollercoaster!")
+height = int(input("Whatt is your heigh in cm? "))
+
+if height > 120:
+    print("You can ride the rollercoaster!")
+else:
+    print ("You cannot ride the rollercoaster!") """
+
+#exercise 1
+number = int(input("Which number do you want to check"))
+
+if number % 2 == 0:
+    print("Even")
+else:
+    print ("Odd")
