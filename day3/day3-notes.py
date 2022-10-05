4d81901c057525ec8b09eb3964c4c9f48b989e7a
Charles <charles@cmcarter.net>
2022-10-05 08:16:30 -0500

    day3 part 1

--- day3/day3-notes.py
+++ day3/day3-notes.py
@@ -9,9 +9,41 @@ else:
     print ("You cannot ride the rollercoaster!") """
 
 #exercise 1
-number = int(input("Which number do you want to check"))
+""" number = int(input("Which number do you want to check"))
 
 if number % 2 == 0:
     print("Even")
 else:
-    print ("Odd")
+    print ("Odd") """
+
+height = float(input("What is your height in m?  "))
+weight = float(input("What is your weight in kg?  "))
+
+bmi = round(weight/(height**2))
+
+
+# if bmi < 35:
+#     if bmi < 18.5:
+#         print("You are underweight")
+#     else:
+#         if bmi < 25:
+#             print("normal weight")
+#         else:
+#             if bmi < 30:
+#                 print("overweight")
+#             else:
+#                 if bmi < 35:
+#                     print("obese")
+# else:
+#     print("clinically obese")
+
+if bmi < 18.5:
+    print(f"Your bmi is {bmi}, you are underweight.")
+elif bmi < 25:
+    print(f"Your bmi is {bmi}, you have a normal weight.")
+else:
+    print(f"Your bmi is {bmi}, you are overweight")
+
+
+
+   
