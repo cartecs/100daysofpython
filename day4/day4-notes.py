f3d48c45f7b7dc0ba0eb3b1bc2e27d52574a0f83
Charles <charles@cmcarter.net>
2022-10-07 07:16:52 -0500

    day 4 part 2

--- day4/day4-notes.py
+++ day4/day4-notes.py
@@ -13,3 +13,26 @@ print(my_module.pi)
 
 ranfloat  = random.random() * 5.1
 print (ranfloat)
+
+fruits = ["apples", "oranges", "blueberries"]
+
+# print(fruits[0])
+# for i in fruits:
+#     print(i)
+
+fruits.append("plum")
+
+# print(fruits[0])
+# for i in fruits:
+#     print(i)
+
+food = []
+food.append(fruits)
+
+print(food)
+
+names = input("Enter some names yo  \n")
+list = names.split(", ")
+list_num = len(list)
+ran_int = random.randint(0,list_num -1)
+print(f"{list[ran_int]} pays the bill.")
