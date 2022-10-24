5d220ac57e23ebbaff949dc4dc3635df6510239d
Charles <charles@cmcarter.net>
2022-10-24 07:51:15 -0500

    day 8 proj part 2 & 3

--- day8/day8-project.py
+++ day8/day8-project.py
@@ -1,10 +1,16 @@
+from re import A
+
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+
 s_alphabet=[]
 encrypt_word=[]
+decrypt_word=[]
+word=[]
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 #shift-=1
@@ -26,8 +32,47 @@ def encrypt(text, shift):
     print(encrypt_word)
     print(''.join(encrypt_word))
 
+def decrypt(text, shift):
+    for y in range(0,len(text)):
+        for w in range(0,26):
+            if text[y]==alphabet[w]:
+                if w-shift > 0:
+                    decrypt_word.append(alphabet[w-shift])
+                else:
+                    decrypt_word.append(alphabet[w-shift+26])
+                print("alphabet letter: " + alphabet[w])
+                print("decrypted letter: " + decrypt_word[y])
 
-encrypt(text, shift)
+def caesar(text, shift):
+    for letter in range(0, len(text)):
+        for alpha in range(0,26):
+            if text[letter]==alphabet[alpha]:
+                if direction == "decrypt":
+                    if alpha-shift > 0:
+                        word.append(alphabet[alpha-shift])
+                    else:
+                        word.append(alphabet[alpha-shift+26])
+                if direction == "encrypt":
+                    if alpha+shift<25:
+                        word.append(alphabet[alpha+shift])
+                    else:
+                        word.append(alphabet[alpha+shift-26])
+    
+    print (f"Your {direction}ed word is: " + ''.join(word))
+
+
+
+
+
+caesar(text, shift)
+#encrypt(text, shift)
+#decrypt(text, shift)
+# if direction == "encrypt":
+#     encrypt(text, shift)
+# elif direction == "decrypt":
+#     decrypt(text, shift)
+# else:
+#     print("Invalid choice. Exiting")
 
 
 
