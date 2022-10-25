9508d50bc541f31ab0a0a81abb601307a30cb62e
Charles <charles@cmcarter.net>
2022-10-25 08:01:22 -0500

    9

--- day9/day9-1.py
+++ day9/day9-1.py
@@ -1,2 +1,10 @@
 kv={"Bug": "An error", "Function": "code you can call"}
 print(kv["Bug"])
+kv["loop"] = "doing something over and over"
+print(kv)
+
+kv["Bug"] = "different value"
+print(kv["Bug"])
+
+for item in kv:
+    print(kv[item])
